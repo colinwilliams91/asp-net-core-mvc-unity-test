f1d84e2e0eca90cef381ed1edfd2f338de9de594
Colin Williams <colin.williams.dev@gmail.com>
2024-06-16 20:07:06 -0500

    feat: define FileUtil class for Get and Truncate file extensions helpers, TODO implement

--- Utilities/CustomStaticFileOptions.cs
+++ Utilities/CustomStaticFileOptions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.StaticFiles;
+﻿using System.Text;
+using Microsoft.AspNetCore.StaticFiles;
 
 namespace asp_net_core_mvc_unity_test.Utilities
 {
@@ -18,57 +19,6 @@ namespace asp_net_core_mvc_unity_test.Utilities
         };
 
         public static IReadOnlyDictionary<string, string> CompressionEncodings => COMPRESSION_ENCODINGS;
-
-        // This handles the MIME type mapping
-        private class CustomContentTypeProvider : IContentTypeProvider
-        {
-            // This by default already knows the MIME type mappings for the most commonly used file types
-            private readonly FileExtensionContentTypeProvider fileTypeProvider = new();
-
-            public CustomContentTypeProvider()
-            {
-                // Can extend it with our own mappings - as mentioned in particular this one
-                fileTypeProvider.Mappings[".data"] = "application/octet-stream";
-            }
-
-            // interface method used when the app tries to find a MIME mapping for a served static file 
-            public bool TryGetContentType(string filePath, out string contentType)
-            {
-                ArgumentNullException.ThrowIfNullOrWhiteSpace(filePath);
-                
-                // If this is a compressed file we need to get the actual extension
-                var extension = Path.GetExtension(filePath);
-                if (extension == GZIP_EXTENSION || extension == BROTLI_EXTENSION)
-                {
-                    // Truncating the .gz or .br compression extension and get the actual extension
-                    // e.g. "Build/something/image.png.gz" -> "Build/something/image.png" -> ".png"
-                    filePath = Path.GetFileNameWithoutExtension(filePath);
-                }
-
-                // then return the according MIME type
-                return fileTypeProvider.TryGetContentType(filePath, out contentType);
-            }
-
-            private static string? GetExtension(string path)
-            {
-                // Don't use Path.GetExtension as that may throw an exception if there are
-                // invalid characters in the path. Invalid characters should be handled
-                // by the FileProviders
-
-                if (string.IsNullOrWhiteSpace(path))
-                {
-                    return null;
-                }
-
-                int index = path.LastIndexOf('.');
-                if (index < 0)
-                {
-                    return null;
-                }
-
-                return path.Substring(index);
-            }
-        }
 
         public static StaticFileOptions GetOptions()
         {
@@ -98,8 +48,73 @@ namespace asp_net_core_mvc_unity_test.Utilities
             ArgumentNullException.ThrowIfNullOrWhiteSpace(extension);
             ArgumentNullException.ThrowIfNullOrWhiteSpace(encoding);
 
-            // Returns 
             return COMPRESSION_ENCODINGS.TryAdd(extension, encoding);
+        }
+
+        // This handles the MIME type mapping
+        private class CustomContentTypeProvider : IContentTypeProvider
+        {
+            // This by default already knows the MIME type mappings for the most commonly used file types
+            private readonly FileExtensionContentTypeProvider fileTypeProvider = new();
+
+            public CustomContentTypeProvider()
+            {
+                // Can extend it with our own mappings - as mentioned in particular this one
+                fileTypeProvider.Mappings[".data"] = "application/octet-stream";
+            }
+
+            // interface method used when the app tries to find a MIME mapping for a served static file 
+            public bool TryGetContentType(string filePath, out string contentType)
+            {
+                ArgumentNullException.ThrowIfNullOrWhiteSpace(filePath);
+
+                // If this is a compressed file we need to get the actual extension
+                var extension = Path.GetExtension(filePath);
+                if (extension == GZIP_EXTENSION || extension == BROTLI_EXTENSION)
+                {
+                    // Truncating the .gz or .br compression extension and get the actual extension
+                    // e.g. "Build/something/image.png.gz" -> "Build/something/image.png" -> ".png"
+                    // TODO: use Substring instead?
+                    filePath = filePath[..^extension.Length];
+                }
+
+                // then return the according MIME type
+                return fileTypeProvider.TryGetContentType(filePath, out contentType);
+            }
+        }
+
+        private static class FileUtilities
+        {
+            /// <summary>
+            /// I elected to use a custom method to get the last file extension including the "." 
+            /// instead of using Path.GetExtension as that may throw an exception if there are
+            /// invalid characters in the path. Invalid characters should be handled by the FileProviders
+            /// </summary>
+            /// <param name="filePath">The string that represents the full file path</param>
+            /// <returns>The last extension in the file path or Null</returns>
+            private static string? GetExtension(string filePath)
+            {
+                if (string.IsNullOrWhiteSpace(filePath))
+                {
+                    return null;
+                }
+
+                int index = filePath.LastIndexOf('.');
+                if (index < 0)
+                {
+                    return null;
+                }
+
+                return filePath.Substring(index);
+            }
+
+            private static string TruncateExtension(string filePath, string extension)
+            {
+                ArgumentNullException.ThrowIfNullOrWhiteSpace(filePath);
+                ArgumentNullException.ThrowIfNullOrWhiteSpace(extension);
+
+                return filePath.Substring(0, filePath.Length - extension.Length);
+            }
         }
     }
 }
