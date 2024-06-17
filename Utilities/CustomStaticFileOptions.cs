2e207e81198d588910c9d79b6eb786a066db27f1
Colin Williams <colin.williams.dev@gmail.com>
2024-06-17 00:41:51 -0500

    refactor: use Path, XML docs, test success, rm hand rolled helpers for string interp

--- Utilities/CustomStaticFileOptions.cs
+++ Utilities/CustomStaticFileOptions.cs
@@ -38,83 +38,74 @@ namespace asp_net_core_mvc_unity_test.Utilities
         }
 
         /// <summary>
-        /// Method to add new compression mappings
+        /// Handles the MIME type mapping for <see cref="StaticFileOptions"/>
+        /// to pass to <see cref="WebApplication.UseStaticFiles"/> middleware pipeline config
+        /// <remarks>
+        /// <see cref="FileExtensionContentTypeProvider"/> IDictionary property already handles
+        /// the MIME type mappings for the 380 most commonly used file types this extends it 
+        /// for Unity WebGL build .data file extension type and according MIME type
+        /// </remarks>
         /// </summary>
-        /// <param name="extension">A File extension like ".gz"</param>
-        /// <param name="encoding">The correct encoding type for the file extension type like "gzip"</param>
-        /// <returns>True if compression mapping added, False if already exists</returns>
-        public static bool AddCompressionEncoding(string extension, string encoding)
-        {
-            ArgumentNullException.ThrowIfNullOrWhiteSpace(extension);
-            ArgumentNullException.ThrowIfNullOrWhiteSpace(encoding);
-
-            return COMPRESSION_ENCODINGS.TryAdd(extension, encoding);
-        }
-
-        // This handles the MIME type mapping
         private class CustomContentTypeProvider : IContentTypeProvider
         {
-            // This by default already knows the MIME type mappings for the most commonly used file types
             private readonly FileExtensionContentTypeProvider fileTypeProvider = new();
 
             public CustomContentTypeProvider()
             {
-                // Can extend it with our own mappings - as mentioned in particular this one
                 fileTypeProvider.Mappings[".data"] = "application/octet-stream";
             }
 
-            // interface method used when the app tries to find a MIME mapping for a served static file 
+            #region Private Class Methods
+            /// <summary>
+            /// Interface method used when the app tries to find a MIME mapping for a served static file
+            /// Leveraging System.IO.Path methods normalizes path separators
+            /// </summary>
+            /// <example>
+            /// Truncating the .gz or .br compression extension and get the actual extension
+            /// "Build\something\image.png.gz" -> "Build\something\image.png" -> ".png"
+            /// </example>
+            /// <remarks>
+            /// Edge Case: <see cref="Path.GetDirectoryName"/>
+            /// "root directory" arg returns null however <see cref="Path.Combine"/>
+            /// leverages <see cref="Path.CombineInternal"/> handling null or empty...
+            /// But I pass string.Empty so the interpreter stops complaining
+            /// </remarks>
+            /// <param name="filePath">Static file path string containing compression extension</param>
+            /// <param name="contentType">
+            /// Code smell out param from eventual .NET<see cref="Dictionary{TKey, TValue}.TryGetValue(TKey, out TValue)"/>
+            /// </param>
+            /// <returns>The according MIME type</returns>
             public bool TryGetContentType(string filePath, out string contentType)
             {
                 ArgumentNullException.ThrowIfNullOrWhiteSpace(filePath);
 
-                // If this is a compressed file we need to get the actual extension
+                // Gets the last "." exentsion (compression type)
                 var extension = Path.GetExtension(filePath);
+                // TODO: extend -- if (COMPRESSION_ENCODINGS.ContainsKey(extension))
                 if (extension == GZIP_EXTENSION || extension == BROTLI_EXTENSION)
                 {
-                    // Truncating the .gz or .br compression extension and get the actual extension
-                    // e.g. "Build/something/image.png.gz" -> "Build/something/image.png" -> ".png"
-                    // TODO: use Substring instead?
-                    filePath = filePath[..^extension.Length];
+                    var filePathDirectory = Path.GetDirectoryName(filePath);
+                    var fileNameTruncated = Path.GetFileNameWithoutExtension(filePath);
+                    filePath = Path.Combine(filePathDirectory ?? string.Empty, fileNameTruncated);
                 }
 
-                // then return the according MIME type
                 return fileTypeProvider.TryGetContentType(filePath, out contentType);
             }
-        }
 
-        private static class FileUtilities
-        {
             /// <summary>
-            /// I elected to use a custom method to get the last file extension including the "." 
-            /// instead of using Path.GetExtension as that may throw an exception if there are
-            /// invalid characters in the path. Invalid characters should be handled by the FileProviders
+            /// Method to add new compression mappings
             /// </summary>
-            /// <param name="filePath">The string that represents the full file path</param>
-            /// <returns>The last extension in the file path or Null</returns>
-            private static string? GetExtension(string filePath)
+            /// <param name="extension">A File extension like ".gz"</param>
+            /// <param name="encoding">The correct encoding type for the file extension type like "gzip"</param>
+            /// <returns>True if compression mapping added, False if already exists</returns>
+            public static bool AddCompressionEncoding(string extension, string encoding)
             {
-                if (string.IsNullOrWhiteSpace(filePath))
-                {
-                    return null;
-                }
-
-                int index = filePath.LastIndexOf('.');
-                if (index < 0)
-                {
-                    return null;
-                }
-
-                return filePath.Substring(index);
-            }
-
-            private static string TruncateExtension(string filePath, string extension)
-            {
-                ArgumentNullException.ThrowIfNullOrWhiteSpace(filePath);
                 ArgumentNullException.ThrowIfNullOrWhiteSpace(extension);
+                ArgumentNullException.ThrowIfNullOrWhiteSpace(encoding);
 
-                return filePath.Substring(0, filePath.Length - extension.Length);
+                return COMPRESSION_ENCODINGS.TryAdd(extension, encoding);
             }
+            #endregion
         }
     }
 }
